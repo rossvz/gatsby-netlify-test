da91d57cda8687153f77076dcfced61da9b048d6
Ross van Zyl <rossvz@getsellr.com>
2018-12-29 18:00:45 -0500

    updated css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
-import React from "react"
-import { Link } from 'gatsby'
+import { Link } from "gatsby";
+import React from "react";
+import styled from "styled-components";
 
-export default () => <div>
-  Hello world!
-  <Link to={"/about"} >About</Link>
-</div>
+const Container = styled.div`
+  display: flex;
+  padding: 5%;
+  text-align: center;
+`;
+
+const Title = styled.div`
+  flex: 1;
+  font-size: 2em;
+`;
+
+export default () => (
+  <Container>
+    <Title>Hello World!</Title>
+    <Link to={"/about"}>About</Link>
+  </Container>
+);
