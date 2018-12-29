4498d715a7da1ffe8c951822af2859359ac5f8a5
Ross van Zyl <rossvz@getsellr.com>
2018-12-29 17:52:32 -0500

    initial

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = (props) => {
+  return (
+    <div>About me!</div>
+  )
+}
+
+export default About
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from 'gatsby'
+
+export default () => <div>
+  Hello world!
+  <Link to={"/about"} >About</Link>
+</div>
